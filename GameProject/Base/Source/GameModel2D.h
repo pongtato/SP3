52e94d476a558813095e55bc37b7626e9fd9d56e
pongtato <forjsa@hotmail.sg>
2015-08-18 00:55:58 +0800

    SP3 Framework

---
+++ GameProject/Base/Source/GameModel2D.h
@@ -0,0 +1,71 @@
+#ifndef GAMEMODEL2D_H
+#define GAMEMODEL2D_H
+
+#include "Model.h"
+
+#include "TileMap.h"
+#include "PlayerCharacter.h"
+
+#include <vector>
+
+class GameModel2D : public Model
+{
+public:
+	enum COMMANDS {
+		MOVE_UP,
+		MOVE_DOWN,
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		JUMP,
+		NUM_COMMANDS,
+	};
+
+protected:
+	enum GEOMETRY_TYPE {
+		TEXT,
+		BACKGROUND,
+		TILE,
+		TILEFLOOR,
+		PLAYER,
+		MOBS,
+		TOTAL_GEOMETRY,
+	};
+
+	Mesh* meshList[TOTAL_GEOMETRY];
+
+	TileMap* m_tileMap;
+	//float m_mapOffset_x;
+	//float m_mapOffset_y;
+
+	PlayerCharacter *player;
+	std::vector<Character *> mobsList;
+
+	int score;
+
+public:
+
+	virtual void Init();
+	virtual void Update(double dt);
+
+	void setCommands(int command);
+
+	Mesh* getTextMesh();
+
+	Mesh* getBackgroundMesh();
+
+	void getOffset(float& mapOffset_x, float& mapOffset_y);
+	TileMap* getTileMap();
+	Mesh* getTileMesh();
+	Mesh* getFloorTileMesh();
+
+	PlayerCharacter* getPlayer();
+	Mesh* getPlayerMesh();
+
+	std::vector<Character *> getMobsList();
+	Mesh* getMobsMesh();
+
+	int getScore();
+
+};
+
+#endif
