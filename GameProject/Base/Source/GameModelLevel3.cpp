81959d9ecbcdb61f04122a353295b3c0173ff0ce
pongtato <forjsa@hotmail.sg>
2015-08-18 16:39:34 +0800

    rear tile map

--- GameProject/Base/Source/GameModelLevel3.cpp
+++ GameProject/Base/Source/GameModelLevel3.cpp
@@ -17,6 +17,10 @@ void GameModelLevel3::Init()
 	m_tileMap->Init(32, 32, 32, 32, 32);
 	m_tileMap->LoadMap("Image//Maps//L3_L1.csv");
 
+	m_ReartileMap = new TileMap();
+	m_ReartileMap->Init(32, 32, 32, 32, 32);
+	m_ReartileMap->LoadMap("Image//Maps//L1_L1.csv");
+
 	player = new PlayerCharacter(Vector3(2, 2, 0));
 	Character *mob = new MouseCharacter(Vector3(16, 3, 0), NULL, -1, player);
 }
